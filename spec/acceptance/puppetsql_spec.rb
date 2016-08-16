2bb8747cfe41ae0b45caf114eaaf218b259552d7
Markus Opolka <markus.opolka@iis.fraunhofer.de>
2016-08-16 10:07:33 +0200

    Add profile and tests for puppetSQL
    A separate Postgresql instance for puppetDB

---
+++ spec/acceptance/puppetsql_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper_acceptance'
+
+describe 'vision_puppet::puppetsql' do
+
+  context 'with defaults' do
+    it 'should idempotently run' do
+      pp = <<-EOS
+        class { 'vision_puppet::puppetsql':
+         listen_address => '0.0.0.0',
+         sql_user       => 'foobar',
+         sql_password   => 'foobar',
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => false)
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+  end
+
+  context 'package installed' do
+    describe package('postgresql-common') do
+      it { should be_installed }
+    end
+  end
+
+end
---
+++ spec/classes/compile_puppetsql_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'hiera'
+
+describe 'vision_puppet::puppetsql' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      context 'compile' do
+        it { is_expected.to compile }
+      end
+
+    end
+  end
+end
