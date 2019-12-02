7d2dc57c42990fc5a35d53c02374ddc153b762d1
Jack Henschel <jackdev@mailbox.org>
2019-12-02 18:41:07 +0100

    Update g10k to v0.8.7
    https://gitlab.cc-asp.fraunhofer.de/vision-it/puppet/control/issues/185

--- manifests/r10k.pp
+++ manifests/r10k.pp
@@ -2,26 +2,14 @@
 # ===========================
 #
 # Profile to manage r10k, but we're using g10k now
-#
-# Parameters
-# ----------
-#
-# @param user Webhook user
-# @param password Webhook password
-#
-# Examples
-# --------
-#
-# @example
-# contain ::vision_puppet::r10k
-#
+# https://github.com/xorpaul/g10k
 
 class vision_puppet::r10k(
 
   String $remote_path_hiera,
   String $remote_path_puppet,
-  String $g10k_url      = 'https://github.com/xorpaul/g10k/releases/download/v0.5.8/g10k-linux-amd64.zip',
-  String $g10k_checksum = '1fee326742e6c90efb23683cbc0063b7dfbdbe1b5a91a98990036a70d5563a33',
+  String $g10k_url      = 'https://github.com/xorpaul/g10k/releases/download/v0.8.7/g10k-linux-amd64.zip',
+  String $g10k_checksum = '7250465ea2d78452a46950ed94a19aa89f218502d17d3c434d6b2af8e06ea399',
 
   ) {
 
@@ -57,6 +45,10 @@ class vision_puppet::r10k(
     mode    => '0644',
     content => template('vision_puppet/r10k.yaml.erb'),
     require => File['/etc/puppetlabs/r10k'],
+  }
+
+  file { '/usr/local/bin/g10k':
+    ensure => absent,
   }
 
 }
